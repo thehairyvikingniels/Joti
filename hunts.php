9717e403d3983e5b3ac298f09f94eb61a30ea800
Niels <thehairyvikingniels@gmail.com>
2023-10-18 13:15:20 +0200

    whoops, fixed allowed areas

--- hunts.php
+++ hunts.php
@@ -115,9 +115,9 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
           <option value="" disabled selected>Kies deelgebeid van hunt</option>
           <option value="Alpha">Alpha</option>
           <option value="Bravo">Bravo</option>
-          <option value="Charlie">Charlie</option>
+          <option value="Charlie" disabled>Charlie</option>
           <option value="Delta" disabled>Delta</option>
-          <option value="Echo" disabled>Echo</option>
+          <option value="Echo">Echo</option>
           <option value="Foxtrot">Foxtrot</option>
         </select>
         <label>Tijd</label>
