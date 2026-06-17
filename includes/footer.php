5821c8d53ac3f2f2935b9f77da07233b622e6b49
Niels <thehairyvikingniels@gmail.com>
2026-06-17 15:52:09 +0200

    Update footer link to point to personal website

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <!-- Footer -->
 <footer class="w3-container w3-padding-16 w3-dark-grey">
-    <center><p><a href="#">Niels Maarleveld</a> - &copy; <?php echo date("Y");?></p></center>
+    <center><p><a href="https://nielsmaarleveld.nl">Niels Maarleveld</a> - &copy; <?php echo date("Y");?></p></center>
 </footer>
