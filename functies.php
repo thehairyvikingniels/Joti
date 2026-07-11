9e40692607337fc7da3e6e740bb92e879eb5285e
Niels <thehairyvikingniels@gmail.com>
2026-07-11 19:36:36 +0200

    Bug: On todo swap it said Hunt: 3 instead of Hunt: Delta
    On todo swap it said Hunt: 3 instead of Hunt: Delta.

--- functies.php
+++ functies.php
@@ -9,6 +9,7 @@ if (empty($_SESSION['id'])){
 }
 
 require("dblogin.php");
+require_once("includes/globals.php");
 
 // GPS Toggle
 if (isset($_GET['gpstoggle'])){
@@ -271,6 +272,8 @@ if (isset($_POST['toggle_toewijzing'])) {
                 $r = $s->get_result();
                 if ($r->num_rows > 0) $c_name = $r->fetch_assoc()['naam'];
                 $s->close();
+            } else if ($c_type == 'hunt') {
+                $c_name = isset($vossen_names[$c_ref]) ? "Hunt: " . $vossen_names[$c_ref] : "Hunt " . $c_ref;
             }
         } else {
             $conflict = $res_car->fetch_assoc();
@@ -304,6 +307,8 @@ if (isset($_POST['toggle_toewijzing'])) {
                 $r = $s->get_result();
                 if ($r->num_rows > 0) $t_name = $r->fetch_assoc()['naam'];
                 $s->close();
+            } else if ($type == 'hunt') {
+                $t_name = isset($vossen_names[$ref_id]) ? "Hunt: " . $vossen_names[$ref_id] : "Hunt " . $ref_id;
             }
 
             echo json_encode(["status" => "conflict", "conflict_name" => $c_name, "target_name" => $t_name]);
